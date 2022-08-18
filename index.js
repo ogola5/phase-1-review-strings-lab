86d56c3ac2e0819197964b5c5312141359476bf4
ogola5 <ogolaevance5@gmail.com>
2022-08-18 11:01:47 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+const currentUser = 'Evance Ogola';
+
+const welcomeMessage = 'Welcome to Flatbook,${Evance Ogola}!';
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = 'Welcome, ${currentUser.slice(0,1)}!';
